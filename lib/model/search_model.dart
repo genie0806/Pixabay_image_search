f7a1004a2bc17d1afb7028b8b1eae6afc120000c
genie0806 <genie0806@gmail.com>
2021-10-06 23:03:24 +0900

    json 데이터를 변환해주는 fetchSearchData 생성.

---
+++ lib/model/search_model.dart
@@ -0,0 +1,15 @@
+import 'dart:convert' as convert;
+import 'package:http/http.dart' as http;
+import 'package:image_search/model/api_model.dart';
+
+const searchImageUrl =
+    'https://pixabay.com/api/?key=17828481-17c071c7f8eadf406822fada3&q=iphone&image_type=photo';
+
+Future<List<SearchModel>> fetchSearchData(String searchWord) async {
+  final response = await http.get(searchImageUrl + '?iphone=$searchImageUrl');
+  Iterable jsonResponse = convert.jsonDecode(response.body);
+  List<SearchModel> searchData =
+      jsonResponse.map((e) => SearchModel.fromJson(e)).toList();
+
+  return searchData;
+}
