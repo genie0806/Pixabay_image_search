f6f8ac1a56a56e7f8bc568af6a79b410f155bbbb
genie0806 <genie0806@gmail.com>
2021-10-06 22:24:08 +0900

    ImageSearch 프로젝트 초기 설정

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:image_search/page/search_page.dart';
+
+void main() {
+  runApp(const ImageSearch());
+}
+
+class ImageSearch extends StatelessWidget {
+  const ImageSearch({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Image Search',
+      home: SearchPage(),
+    );
+  }
+}
