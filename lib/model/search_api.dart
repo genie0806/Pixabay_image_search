4641de5da36aaa426ceae3c1bc6dea3ac8ed96b9
genie0806 <genie0806@gmail.com>
2021-10-14 01:06:03 +0900

    생성자 reqired, json model의 클래스 변수에 late 기재하기

--- lib/model/search_api.dart
+++ lib/model/search_api.dart
@@ -1,17 +1,18 @@
 import 'package:image_search/model/saerch_data.dart';
 
 class SearchModel {
-  int total;
-  int totalHits;
-  List<Hits> hits;
+  late int total;
+  late int totalHits;
+  late List<Hits> hits;
 
-  SearchModel({this.total, this.totalHits, this.hits});
+  SearchModel(
+      {required this.total, required this.totalHits, required this.hits});
 
   SearchModel.fromJson(Map<String, dynamic> json) {
     total = json['total'];
     totalHits = json['totalHits'];
     if (json['hits'] != null) {
-      hits = List<Hits>();
+      hits = [];
       json['hits'].forEach((v) {
         hits.add(new Hits.fromJson(v));
       });
--- lib/model/search_model.dart
+++ lib/model/search_model.dart
@@ -6,14 +6,14 @@ import 'package:image_search/model/search_api.dart';
 class PixaBayApi {
   static const base_url = 'https://pixabay.com/api/';
 
-  Future<List<Hits>> fetchSearchData(String searchWord) async {
-    final response = await http.get(
-        '$base_url?key=17828481-17c071c7f8eadf406822fada3&q=$searchWord&image_type=photo');
+  Future<List<Hits>/*!*/> fetchSearchData(String searchWord) async {
+    final response = await http.get(Uri.parse(
+        '$base_url?key=17828481-17c071c7f8eadf406822fada3&q=$searchWord&image_type=photo'));
     if (response.statusCode == 200) {
       Map<String, dynamic> jsonResponse = convert.jsonDecode(response.body);
       SearchModel searchdata = SearchModel.fromJson(jsonResponse);
       return searchdata.hits;
     }
-    throw FormatException('statusCode is not 200');
+    throw Exception('statusCode is not 200');
   }
 }
