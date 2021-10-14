44e6e63318dccdf2839fc7cd5fc0a5fb1e72398c
genie0806 <genie0806@gmail.com>
2021-10-15 04:20:50 +0900

    build_runner가 되지않음

--- lib/model/saerch_data.dart
+++ lib/model/saerch_data.dart
@@ -1,73 +1,22 @@
+import 'package:json_annotation/json_annotation.dart';
+
+part 'hits.g.dart';
+
+@JsonSerializable()
 class Hits {
-  late int id;
-  late String pageURL;
-  late String type;
-  late String tags;
-  late String previewURL;
-  late int previewWidth;
-  late int previewHeight;
-  late String webformatURL;
-  late int webformatWidth;
-  late int webformatHeight;
-  late String largeImageURL;
-  late int imageWidth;
-  late int imageHeight;
-  late int imageSize;
-  late int views;
-  late int downloads;
-  late int collections;
-  late int likes;
-  late int comments;
-  late int userId;
-  late String user;
-  late String userImageURL;
+  String pageURL;
+  String tags;
+  String previewURL;
+  String webformatURL;
+  int views;
+  int downloads;
+  int likes;
+  int comments;
+  String user;
 
-  Hits(
-      {required this.id,
-      required this.pageURL,
-      required this.type,
-      required this.tags,
-      required this.previewURL,
-      required this.previewWidth,
-      required this.previewHeight,
-      required this.webformatURL,
-      required this.webformatWidth,
-      required this.webformatHeight,
-      required this.largeImageURL,
-      required this.imageWidth,
-      required this.imageHeight,
-      required this.imageSize,
-      required this.views,
-      required this.downloads,
-      required this.collections,
-      required this.likes,
-      required this.comments,
-      required this.userId,
-      required this.user,
-      required this.userImageURL});
+  Hits(this.pageURL, this.comments, this.downloads, this.previewURL, this.likes,
+      this.tags, this.user, this.views, this.webformatURL);
 
-  Hits.fromJson(Map<String, dynamic> json) {
-    id = json['id'];
-    pageURL = json['pageURL'];
-    type = json['type'];
-    tags = json['tags'];
-    previewURL = json['previewURL'];
-    previewWidth = json['previewWidth'];
-    previewHeight = json['previewHeight'];
-    webformatURL = json['webformatURL'];
-    webformatWidth = json['webformatWidth'];
-    webformatHeight = json['webformatHeight'];
-    largeImageURL = json['largeImageURL'];
-    imageWidth = json['imageWidth'];
-    imageHeight = json['imageHeight'];
-    imageSize = json['imageSize'];
-    views = json['views'];
-    downloads = json['downloads'];
-    collections = json['collections'];
-    likes = json['likes'];
-    comments = json['comments'];
-    userId = json['user_id'];
-    user = json['user'];
-    userImageURL = json['userImageURL'];
-  }
+  factory Hits.fromJson(Map<String, dynamic> json) => _$HitsFromJson(json);
+  Map<String, dynamic> toJson() => _$HitsToJson(this);
 }
--- lib/model/search_api.dart
+++ lib/model/search_api.dart
@@ -1,21 +1,14 @@
 import 'package:image_search/model/saerch_data.dart';
 
+part 'search_model.g.dart';
+
 class SearchModel {
   late int total;
   late int totalHits;
   late List<Hits> hits;
 
-  SearchModel(
-      {required this.total, required this.totalHits, required this.hits});
-
-  SearchModel.fromJson(Map<String, dynamic> json) {
-    total = json['total'];
-    totalHits = json['totalHits'];
-    if (json['hits'] != null) {
-      hits = [];
-      json['hits'].forEach((v) {
-        hits.add(Hits.fromJson(v));
-      });
-    }
-  }
+  SearchModel(this.hits, this.total, this.totalHits);
+  factory SearchModel.fromJson(Map<String, dynamic> json) =>
+      _$SearchModelFromJson(json);
+  Map<String, dynamic> toJson() => _$SearchModelToJson(this);
 }
